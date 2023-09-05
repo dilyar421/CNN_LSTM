63158c10400d35e273a043d496448392e97393bd
dilyar421 <144108461+dilyar421@users.noreply.github.com>
2023-09-05 16:47:06 +0800

    Add files via upload
    CNN_LSDM_F1.py 是以F1为目标函数的贝叶斯优化参数搜索的结果应用到了总模型中，CNN_LSTM_ACCURACY.py是以准确率为目标函数，剩下的时原始数据

---
+++ CNN_LSTM_ACCURACY.py
@@ -0,0 +1,103 @@
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from keras.models import Sequential
+from keras.layers import Conv1D, MaxPooling1D, LSTM, Dense, Flatten, LeakyReLU, Dropout
+from keras.callbacks import EarlyStopping
+from keras import regularizers
+from keras.utils import to_categorical
+import pandas as pd
+import numpy as np
+from keras.metrics import Precision, Recall
+from keras import backend as K
+from keras.optimizers import Adam
+
+# 数据预处理
+train_data = pd.read_csv('train_data.txt', sep="\t")
+val_data = pd.read_csv('val_data.txt', sep="\t")
+test_data = pd.read_csv('test_data.txt', sep="\t")
+
+train_labels = pd.read_csv("train_data_labels.txt", sep="\t")
+val_labels = pd.read_csv("val_data_labels.txt", sep="\t")
+test_labels = pd.read_csv("test_data_labels.txt", sep="\t")
+
+encoder = OneHotEncoder(sparse=False)
+train_labels = encoder.fit_transform(train_labels)
+val_labels = encoder.transform(val_labels)
+test_labels = encoder.transform(test_labels)
+
+scaler = StandardScaler()
+train_data_scaled = scaler.fit_transform(train_data)
+val_data_scaled = scaler.transform(val_data)
+test_data_scaled = scaler.transform(test_data)
+
+class_weight_dict = {0: 0.0964, 1: 0.5594, 2: 0.1712, 3: 0.1274, 4: 0.0456}
+
+# 模型构建
+model = Sequential()
+
+# 1D-CNN 层
+model.add(Conv1D(filters=56, kernel_size=2, activation='relu',
+                 kernel_regularizer=regularizers.l1_l2(l1=1.0805843791263926e-06, l2=4.540555326102059e-06),
+                 input_shape=(9, 1)))
+
+# MaxPooling 层
+model.add(MaxPooling1D(pool_size=2))
+
+# 额外的 1D-CNN 层
+model.add(Conv1D(filters=16, kernel_size=4, padding='same', strides=1))
+model.add(LeakyReLU(alpha=0.3))
+model.add(MaxPooling1D(pool_size=2))
+
+# LSTM 层
+model.add(LSTM(units=70, return_sequences=True, activation='relu',
+               kernel_regularizer=regularizers.l1_l2(l1=4.508324743209948e-06, l2=5.9424260748720894e-06),
+               dropout=0.28656904517561677, recurrent_dropout=0.20555067255968323))
+
+model.add(LSTM(units=65, return_sequences=False, activation='relu',
+               dropout=0.1403052882218182, recurrent_dropout=0.13291036538230594))
+
+# Dropout 层
+model.add(Dropout(0.17655838795982484))
+
+# 输出层
+model.add(Dense(5, activation='softmax'))
+
+# F1 Score Function
+def f1_score(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    recall = true_positives / (possible_positives + K.epsilon())
+    f1_val = 2 * (precision * recall) / (precision + recall + K.epsilon())
+    return f1_val
+
+# 编译模型
+optimizer = Adam(learning_rate=0.0005470817929946867)
+model.compile(optimizer=optimizer, loss='categorical_crossentropy',
+              metrics=['accuracy', Precision(), Recall(), f1_score])
+
+# 重新塑造数据以适应模型
+train_data_scaled = train_data_scaled.reshape((train_data_scaled.shape[0], 9, 1))
+val_data_scaled = val_data_scaled.reshape((val_data_scaled.shape[0], 9, 1))
+test_data_scaled = test_data_scaled.reshape((test_data_scaled.shape[0], 9, 1))
+
+# 使用早停法防止过拟合
+early_stopping = EarlyStopping(monitor='val_loss', patience=6)
+
+# 训练模型
+history = model.fit(train_data_scaled, train_labels, epochs=50, batch_size=64, 
+                    validation_data=(val_data_scaled, val_labels), 
+                    callbacks=[early_stopping], class_weight=class_weight_dict)
+
+# 输出指标
+print("Validation Accuracy:", max(history.history['val_accuracy']))
+print("Validation Precision:", max(history.history['val_precision']))
+print("Validation Recall:", max(history.history['val_recall']))
+print("Validation F1 Score:", max(history.history['val_f1_score']))
+
+# 评估模型
+loss, accuracy, precision, recall, f1score = model.evaluate(test_data_scaled, test_labels)
+print(f"Test loss: {loss}, Test accuracy: {accuracy}, Test Precision: {precision}, Test Recall: {recall}, Test F1 Score: {f1score}")
+
+# 保存模型
+model.save('my_trained_model_accuracy.h5')
